59ae7e8a90dfebc57b110914696c031315b07e73
Camille Fujisaki <cae224@cornell.edu>
2023-04-07 10:29:59 +0900

    logic sans styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import useGame from "./hooks/useGame"
 
 function App() {
+  const {
+      textBoxRef,
+      handleChange,
+      text,
+      isTimeRunning,
+      timeRemaining,
+      startGame,
+      wordCount
+  } = useGame(5)
+
   return (
     <div className="App">
+      <h1>How fast do you type?</h1>
+      <textarea
+        ref={textBoxRef}
+        onChange={handleChange}
+        value={text}
+        disabled={!isTimeRunning}/>
+      <h4>Time remaining: {timeRemaining}</h4>
+      <button
+        onClick={startGame}
+        disabled={isTimeRunning}
+      >
+        Start
+      </button>
+      <h1>Word Count: {wordCount}</h1>
     </div>
   );
 }
